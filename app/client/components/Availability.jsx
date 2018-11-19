d7622cf2b921f557ac7ea7b8e6e7494e2cb307dc
Oliver Jägle <github@beimir.net>
2018-11-19 20:58:01 +0100

    Availabilities with pot. avatar

--- app/client/components/Availability.jsx
+++ app/client/components/Availability.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Container from 'react-bulma-components/lib/components/container';
+import Image from 'react-bulma-components/lib/components/image';
+import Media from 'react-bulma-components/lib/components/media';
+import Columns from 'react-bulma-components/lib/components/columns';
+import User from './User';
 
 export default function Availabilities(props) {
   const {
@@ -11,15 +15,23 @@ export default function Availabilities(props) {
     <Container className="c-availabilities">
       <Heading size={3}>Verfügbarkeit</Heading>
 
-      <ul>
+      <Columns multiline centered>
         {
         members.map(member => (
-          <li key={member.username}>
-            {`${member.username}: ${member.availability}`}
-          </li>
+          <Columns.Column size={3}>
+            <Media>
+              <Media.Item renderAs="figure" position="left">
+                <Image renderAs="p" size={64} alt="64x64" src="http://bulma.io/images/placeholders/128x128.png" />
+              </Media.Item>
+              <Media.Item>
+                <User username={member.username} />
+                <p>{member.availability}</p>
+              </Media.Item>
+            </Media>
+          </Columns.Column>
         ))
       }
-      </ul>
+      </Columns>
     </Container>
   );
 }
