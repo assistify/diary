ff52e4701fb0770f22a0c11fe15c1e9cd7b1472b
Oliver Jägle <github@beimir.net>
2018-11-25 00:42:51 +0100

    File structure similar to create-react-app

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import './styles/index.scss';
 import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
-render(
-  <App />,
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
