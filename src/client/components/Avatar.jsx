31c00d084035aa89b52dc049c5e22cc88e384118
Oliver Jägle <github@beimir.net>
2018-11-28 16:47:10 +0100

    Fix typo

--- src/client/components/Avatar.jsx
+++ src/client/components/Avatar.jsx
@@ -6,25 +6,31 @@ import { TeamContext } from '../lib/teamContext';
 
 export default class Avatar extends Component {
     static propTypes = {
-      username: PropTypes.string.isRequired
+      username: PropTypes.string.isRequired,
+      size: PropTypes.number,
     }
 
     static contextType = TeamContext;
 
+    static defaultProps = {
+      size: 128
+    }
+
     constructor(props) {
       super(props);
       this.state = {
-        username: props.username
+        username: props.username,
+        size: props.size
       };
     }
 
     render() {
-      const { username } = this.state;
+      const { username, size } = this.state;
       return (
 
         <TeamContext.Consumer>
           {teamContext => (
-            <Image size={128} className="avatar" alt={username} src={`${teamContext.serverUrl}/avatar/${username}`} fallback="http://bulma.io/images/placeholders/128x128.png" />
+            <Image size={size} className="avatar" alt={username} src={`${teamContext.serverUrl}/avatar/${username}`} fallback="http://bulma.io/images/placeholders/128x128.png" />
           )}
         </TeamContext.Consumer>
       );
