97d5f191c6f3075bbafb2f7e49926b2a418a8c82
Oliver Jägle <github@beimir.net>
2019-02-12 14:29:47 +0100

    Change Label of "blockiert"

---
+++ src/client/components/BlockedIndication.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tag from 'react-bulma-components/lib/components/tag';
+
+export default function BlockedIndication(/*props */) {
+  return (
+    <Tag className="blocked">
+      {/* <span role="img" aria-label="blocked">
+        ⛔️
+      </span> */}
+      Hilfe!
+    </Tag>
+  );
+}
